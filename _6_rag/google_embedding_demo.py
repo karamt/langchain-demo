2f4d9a7d606ac7d580287f8619bb3aed9fdc3010
karamt <teckaram@gmail.com>
2025-01-03 21:22:23 -0600

    adding files for langchain demo

--- _6_rag/google_embedding_demo.py
+++ _6_rag/google_embedding_demo.py
@@ -1,8 +1,5 @@
 import google.generativeai as genai
 
-GOOGLE_API_KEY = "AIzaSyB-i9YL2_PzzmEIMBZTNoOc-wTJrqwGjcA"
-genai.configure(api_key=GOOGLE_API_KEY)
-
 result = genai.embed_content(
         model="models/text-embedding-004",
         content="What is the meaning of life?")
